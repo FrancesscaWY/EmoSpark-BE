1895f7a44ba2620183d00602f5893c606d52e42d
francessca <3449997118@qq.com>
2025-05-07 15:01:51 +0800

    -add API test

--- dataBase/initDB.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('./DB/example.db')
-cursor = conn.cursor()
-cursor.execute('CREATE TABLE IF NOT EXISTS '
-               'users (id INTEGER PRIMARY KEY, name TEXT)')
-cursor.execute('INSERT INTO users (name) VALUES (?)', ('Alice',))
-conn.commit()
-conn.close()
